377b929a060da8fca557e751097c18d5180c436a
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-10-28 00:06:57 +0300

    Fire emitter is added (particle system)

---
+++ Assets/Scripts/Classes/ForestTree.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestTree
+{
+    public GameObject tree;
+    public GameObject burnedIcon;
+    public List<GameObject> closestTrees = new List<GameObject>();
+    public bool Burned;
+
+    public ForestTree()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Generators/FireGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FireGenerator
+{
+    public static GameObject fireEmitter;
+    public static GameObject smokeEmmiter;
+
+
+    public static void StartFire(ForestTree forestTree)
+    {
+
+    }
+
+    public static void SpreadFire(ForestTree forestTree)
+    {
+
+    }
+}
